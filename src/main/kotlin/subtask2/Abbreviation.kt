3664a5144b75df30b8ff1204c2d81a0de018ee2c
T. Ustiugov <utmarckus@gmail.com>
2025-08-07 10:04:37 +0300

    feat: add abbreviation solution

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,7 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+    fun abbreviationFromA(a: String, b: String): String = a.filter { b.contains(it.toUpperCase()) }.let {
+        if (it.toUpperCase() == b) "YES" else "NO"
     }
 }
